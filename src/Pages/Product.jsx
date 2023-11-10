e6b564b8e375b0389e5b4b49e42222638ee01b98
samarpan sarkar <samarpansarkar209@gmail.com>
2023-11-10 22:19:47 +0530

    2.2.0

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
-import all_product from "./../Components/Assets/all_product";
 import { ShopContext } from "./../Context/ShopContext";
 import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrum/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
   return <div>
-    
+    <Breadcrum product = {product}/>
+    <ProductDisplay product={product}/>
   </div>;
 };
 
